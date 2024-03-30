fe7059c8bda094bd5a81e895b90c823af5744f29
JosaelPrz <josael.prz@gmail.com>
2024-03-30 06:06:37 -0700

    Set a better project structure and examples

--- src/lib/components/Component.tsx
+++ src/lib/components/Component.tsx
@@ -1,5 +1,22 @@
-function Component() {
-  return <h1>React</h1>
+import logo from "@assets/media/vectors/react.svg"
+
+type Props = {
+  field?: string
+  field2: boolean
+}
+
+function Component({ field = "defaultValue", field2 }: Props) {
+  return (
+    <>
+      <div className="m-4 flex gap-4">
+        <img src={logo} alt="React Logo" />
+        <h1>
+          <span className="font-bold">React</span> - {field}
+        </h1>
+        {field2 && <span>Conditional element</span>}
+      </div>
+    </>
+  )
 }
 
 export default Component
